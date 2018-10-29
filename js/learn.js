ca52014ea2b7fe7146e089579eb9f40bc0c77a96
nattila12 <43964738+nattila12@users.noreply.github.com>
2018-10-29 20:27:15 +0200

    add string variables

---
+++ js/learn.js
@@ -0,0 +1,16 @@
+console.info(0);
+
+var nume = "Attila";
+var prenume = "Nagy";
+var owner = "Attila's";
+var msg1 = 'ai zis "Nick"?';
+var msg2 = 'Nu, am zis "Attila"';
+var template = `Text cu ghilimele "sau simple'`;
+
+var skills = ["html", "css", "js"];
+
+prenume = "Nick";
+
+console.info("toate variabilele au fost initializate");
+console.log(nume);
+console.debug("cum te cheama? -" + prenume);
