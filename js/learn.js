3da5673d89111c31eb4d691475b39f74789c95e8
nattila12 <43964738+nattila12@users.noreply.github.com>
2018-11-05 20:15:29 +0200

    add getExtractFee add var global , add ExtractFromATMFunds , add checkExtractPermission

--- js/learn.js
+++ js/learn.js
@@ -35,18 +35,50 @@ console.info("eu am " + myRon + "Ron");
 friendRon = friendEur * cursEur - (friendEur * 1) / 100;
 console.info("tu ai " + friendRon + "Ron");
 
-function extractFromAtm(ammount) {
-  console.info("==== ==== ==== ====");
-  console.info("suma extrasa este: " + ammount);
-  var comision = ammount * 0.01;
+var ATMFunds = 50000;
+var funds = 5000;
+
+function getExtractFee(amount) {
+  var comision = amount * 0.01;
   if (comision < 2.5) {
     comision = 2.5;
     console.warn("comision minim aplicat");
   }
+  return comision;
+}
+
+//TODO implement
+function checkExtractPermission() {
+  return true;
+}
+
+function extractFromAtm(amount) {
+  console.info("==== ==== ==== ====");
+  console.info("suma extrasa este: " + amount);
+  var comision = getExtractFee(amount);
+
+  var totalExtract = amount + comision;
+
+  if (totalExtract > funds) {
+    console.error("insufficient funds");
+    return;
+  }
+  if (ATMFunds < totalExtract) {
+    console.error("ATM insufficient funds");
+    return;
+  }
+  funds = funds - totalExtract;
+  //ATMFunds = ATMFunds - ammount;
+  ATMFunds -= amount;
+
   console.info("comision aplicat:" + comision);
+  console.info("sold curent:" + funds);
   console.info("==== ==== ==== ====");
   console.info("==== ==== ==== ====");
 }
 
-extractFromAtm(1000);
+extractFromAtm(1005); // nu e multiplu de 10
+extractFromAtm(100000); // solduri insuficiente
+extractFromAtm(3000);
+extractFromAtm(3000);
 extractFromAtm(100);
