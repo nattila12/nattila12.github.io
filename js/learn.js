cabff6e7f131c8298d4dea04adc15f5cbf2af3d2
nattila12 <43964738+nattila12@users.noreply.github.com>
2018-11-05 20:23:16 +0200

    move learnVariables in external function

--- js/learn.js
+++ js/learn.js
@@ -1,25 +1,28 @@
-console.info(0);
+function learnVariables() {
+  console.info(0);
 
-function sayWellcome() {
-  console.info("Salut " + prenume + ",bine ai venit la noi!");
+  function sayWellcome() {
+    console.info("Salut " + prenume + ",bine ai venit la noi!");
+  }
+
+  var nume = "Attila";
+  var prenume = "Nagy";
+  var owner = "Attila's";
+  var msg1 = 'ai zis "Atty"?';
+  var msg2 = 'Nu, am zis "Attila"';
+  var template = `Text cu ghilimele "sau simple'`;
+  sayWellcome();
+
+  var skills = ["html", "css", "js"];
+
+  prenume = "Atty";
+
+  console.info("toate variabilele au fost initializate");
+  console.log(nume);
+  console.debug("cum te cheama? -" + prenume);
+  sayWellcome();
 }
-
-var nume = "Attila";
-var prenume = "Nagy";
-var owner = "Attila's";
-var msg1 = 'ai zis "Atty"?';
-var msg2 = 'Nu, am zis "Attila"';
-var template = `Text cu ghilimele "sau simple'`;
-sayWellcome();
-
-var skills = ["html", "css", "js"];
-
-prenume = "Atty";
-
-console.info("toate variabilele au fost initializate");
-console.log(nume);
-console.debug("cum te cheama? -" + prenume);
-sayWellcome();
+//learnVariables();
 
 //schimb valutar
 var cursEur = 4.5;
