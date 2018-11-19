df8303fd12ac9a8df4132dceb1d18cd53d3f5d89
Nagy Attyla <nagyattyla@Attilas-MBP.localdomain>
2018-11-19 19:17:22 +0200

    add show/hide functions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 
+function hide(id){
+  document.getElementById(id).style.display = "none";
+}
+
+function show(id){
+  document.getElementById(id).style.display = "block";
+}
+
+
+
+
 
 function showHomePage() {
-  document.getElementById("skills-page").style.display = "none";
-  document.getElementById("languages-page").style.display = "none";
-  document.getElementById("home-page").style.display = "block";
+  hide("skills-page");
+  hide("languages-page");
+  show("home-page");
 }
 
 
 function showSkillsPage() {
-  document.getElementById("home-page").style.display = "none";
-  document.getElementById("languages-page").style.display = "none";
-  document.getElementById("skills-page").style.display = "block";
+  hide("home-page");
+  hide("languages-page");
+  show("skills-page");
 
 }
 
 
 function showLimbiCunoscutePage() {
-  document.getElementById("home-page").style.display = "none";
-  document.getElementById("skills-page").style.display = "none";
-  document.getElementById("languages-page").style.display = "block";
+  hide("home-page");
+  hide("skills-page");
+  show("languages-page");
 }
 
 
