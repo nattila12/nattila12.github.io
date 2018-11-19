bb83b99fbd3b5561b052f2cd552e3db06d93da2c
Nagy Attyla <nagyattyla@Attilas-MBP.localdomain>
2018-11-19 21:04:27 +0200

    add hideAllPages

--- src/index.js
+++ src/index.js
@@ -12,41 +12,23 @@ function show(id){
 
 
 
-function showHomePage() {
-  hide("skills-page");
-  hide("education-page");
-  hide("languages-page");
-  show("home-page");
+function hideAllPages(){
+  var pages = document.querySelectorAll(".page-block");
+  for (var i=0; i<pages.length; i++){
+    pages[i].style.display = 'none'
+  }
 }
 
 
-function showSkillsPage() {
-  hide("home-page");
-  hide("education-page");
-  hide("languages-page");
-  show("skills-page");
-
+var links = document.querySelectorAll("#top-menu-bar a");
+for(var i=0; i<links.length; i++ ){
+  //console.log(links[i].getAttribute('data-page'), links[i]);
+  links[i].onclick = function(){
+    console.warn('hide all pages');
+    hideAllPages();
+    var page = this.getAttribute('data-page');
+    show(page + "-page");
+  }
 }
 
-function showEducationPage() {
-  hide("home-page");
-  hide("skills-page");
-  hide("languages-page");
-  show("education-page");
-
-
-}
-
-
-function showLimbiCunoscutePage() {
-  hide("home-page");
-  hide("skills-page");
-  hide("education-page");
-  show("languages-page");
-}
-
-
-$("home-menu").onclick = showHomePage;
-$("skills-menu").onclick = showSkillsPage;
-$("education-menu").onclick = showEducationPage;
-$("languages-menu").onclick = showLimbiCunoscutePage;
+show("home-page");
