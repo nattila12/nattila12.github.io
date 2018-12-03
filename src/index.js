cef42f80400b01e3d3e04e42603e333862fb320a
Nagy Attyla <nagyattyla@Attilas-MBP.localdomain>
2018-12-03 20:14:00 +0200

    add json format for skill

--- src/index.js
+++ src/index.js
@@ -38,21 +38,21 @@ function initMenu(){
 
 function initSkillsPage(){
   var skills = [
-    ['js', 7, "Andrei"],
-    ['html', 6, "Andreea"] , 
-    ['css', 2, ""]
+    {name: 'js', endorsments:7, endorsedBy: 'Andrei'},
+    {name: 'html', endorsments:6, endorsedBy: 'Andrea'},
+    {name: 'css', endorsments:2, endorsedBy: ''}
   ];
   var resultList = document.querySelector('#skills-page ul');
 
   var listItems = skills.map(function(skill) {
     var endorsedBy = '- Endorsed by';
-    if(skill[2] == ""){
+    if(skill.endorsedBy == ""){
       endorsedBy = "";
     }
-    var name = skill[0].toUpperCase();
+    var name = skill.name.toUpperCase();
     return `<li> ${name}
-     <span style="color:gray">- ${skill[1]} ${endorsedBy}</span> 
-     ${skill[2]}</li>`;
+     <span style="color:gray">- ${skill.endorsments} ${endorsedBy}</span> 
+     ${skill.endorsedBy}</li>`;
   });
 
   resultList.innerHTML = listItems.join('');
